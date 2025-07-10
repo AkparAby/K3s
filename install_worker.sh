2f57069f182551ccc07818d21e2e6ae574de059a
AkparAby <AkparAby@outlook.com>
2025-07-10 10:33:10 +0800

    添加worker节点安装脚本

---
+++ install_worker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+echo "🧹 卸载旧的 k3s-agent..."
+/usr/local/bin/k3s-agent-uninstall.sh || true
+rm -rf /etc/rancher /var/lib/rancher /var/lib/kubelet /run/k3s
+
+echo "🛠️ 配置国内镜像加速..."
+mkdir -p /etc/rancher/k3s
+
+cat > /etc/rancher/k3s/registries.yaml <<EOF
+mirrors:
+  "docker.io":
+    endpoint:
+      - "https://docker.1panel.live"
+      - "https://docker.1ms.run"
+      - "https://dytt.online"
+      - "https://docker-0.unsee.tech"
+      - "https://lispy.org"
+      - "https://666860.xyz"
+      - "https://hub.rat.dev"
+      - "https://docker.m.daocloud.io"
+      - "https://demo.52013120.xyz"
+      - "https://proxy.vvvv.ee"
+EOF
+
+if [ -z "$K3S_URL" ] || [ -z "$K3S_TOKEN" ]; then
+  echo "请设置环境变量 K3S_URL 和 K3S_TOKEN"
+  exit 1
+fi
+
+echo "🚀 安装 k3s Worker，加入集群..."
+curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn K3S_URL=$K3S_URL K3S_TOKEN=$K3S_TOKEN sh -
